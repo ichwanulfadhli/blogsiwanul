cd1f2e59b94fd6e1b55b8792646ea4c9a5d4ee4c
Ichwanul Fadhli <ichwanulf60@gmail.com>
2020-05-04 04:32:55 +0700

    Memperbaiki "bug" tombol back

--- application/views/blog/_blog_view.php
+++ application/views/blog/_blog_view.php
@@ -225,7 +225,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 		});
 
 		$(".back").click(function() {
-			history.back();
+			if(document.referrer == ''){
+				window.location.href = "<?php echo site_url(); ?>";
+			}
+			else{
+				history.back();
+			}
 		});
 	</script>
 	<!-- End of control center -->
