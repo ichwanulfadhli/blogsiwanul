eb2000abdb6ed832ec8162a0378f784b11708bc1
Ichwanul Fadhli <ichwanulf60@gmail.com>
2020-05-27 17:06:14 +0700

    Konfigurasi CKEditor

--- assets/js/plugins/ckeditor/config.js
+++ assets/js/plugins/ckeditor/config.js
@@ -7,23 +7,41 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For complete reference see:
 	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+	
+	// Config extra plugins
+	config.extraPlugins = 'wordcount,notification';
+	// Config wordcount
+	config.wordcount = {
+
+		// Whether or not you want to show the Word Count
+		showWordCount: false,
+	
+		// Whether or not you want to show the Char Count
+		showCharCount: true,
+		
+		// Maximum allowed Word Count
+		maxWordCount: -1,
+	
+		// Maximum allowed Char Count
+		maxCharCount: 500,
+	};
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'links' },
+		// { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		// { name: 'insert' },
+		// { name: 'forms' },
+		// { name: 'tools' },
+		// { name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		// { name: 'others' },
+		// '/',
+		// { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		// { name: 'styles' },
+		// { name: 'colors' },
+		// { name: 'about' }
 	];
 
 	// Remove some buttons provided by the standard plugins, which are
