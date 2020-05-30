209cc8cfc6da52789660569dbf44699218fc859e
Ichwanul Fadhli <ichwanulf60@gmail.com>
2020-05-30 11:07:08 +0700

    Mengganti proses pengambilan data dengan curl

--- application/libraries/Api.php
+++ application/libraries/Api.php
@@ -3,29 +3,56 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 Class Api {
 
-    public function callAPI($method, $url, $data = NULL){
-        if($method == "GET" && $data === NULL){
-            $content = file_get_contents($url, false);
+    /**
+     * Call API Function. To get a resource from an API with a certain request.
+     * 
+     * @param string $method 
+     * The request method : "GET", "POST", "PUT", "DELETE".
+     * 
+     * @param string $url 
+     * The specified API's url.
+     * 
+     * @param array $data 
+     * The data sent request. By default it's set to _null_, but if you wish to make "POST" or "PUT" request, 
+     * the value shall be an array.
+     * 
+     */
+    public function callAPI($method, $url, $data = null){
+        // Initialize curl
+        $curl = curl_init();
 
-            return json_decode($content, true);
+        // Checking the method request
+        switch($method){
+            // POST request
+            case "POST":
+                curl_setopt($curl, CURLOPT_POST, 1);
+                if ($data)
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                break;
+            // PUT request
+            case "PUT":
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+                if ($data)
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                break;
+            default:
+                if ($data)
+                    $url = sprintf("%s?%s", $url, http_build_query($data));
         }
-        elseif($method == "POST" || $method == "PUT"){
-            $opts = array('http' => 
-            array(
-                'method'    => $method,
-				'header'    => 'Content-Type: application/x-www-form-urlencoded',
-                'content'   => $data
-                )
-            );
-            
-            $context = stream_context_create($opts);
-        
-            $content = file_get_contents($url, false, $context);
-            
-            return json_decode($content, true);
+        // Options
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+        // Execute the process
+        $result = curl_exec($curl);
+        if (!$result) {
+            die("Connection Failure");
         }
-        else{
-            return false;
-        }
+        curl_close($curl);
+
+        // Returning the output
+        return json_decode($result, true);
     }
 }
